8209f78ff49f92c5327f911288e298f74625eeb3
trushna116 <trushnabadwaik572@gmail.com>
2023-07-11 14:24:22 +0530

    scripthfile added

---
+++ scripthfile.js
@@ -0,0 +1,29 @@
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(() =>{
+      resolve("value 1");
+    },5000)
+  })
+  let p2 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve("value 2")
+      //reject(new Error("error"));
+    },2000);
+  });
+  
+  let p3 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve("value 3")
+    },3000);
+  })
+  
+  
+  //which promise is first executed will give this
+  let promise_all = Promise.race([p1,p2,p3])
+  promise_all.then((value)=>{
+    console.log(value)
+  })
+  
+  let promise_error = Promise.reject(new Error("hey"))
+  promise_error.then((value)=>{
+    console.log(value)
+  })
